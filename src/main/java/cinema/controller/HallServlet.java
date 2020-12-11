d4579946f5f6a42f70b295b192cc7cc7a02bcd55
Vitaly Yagufarov <for.viy@gmail.com>
2020-12-11 18:54:32 +0500

    Add html files

---
+++ src/main/java/cinema/controller/HallServlet.java
@@ -0,0 +1,21 @@
+package cinema.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Class HallServlet.
+ *
+ * @author Vitaly Yagufarov (for.viy@gmail.com)
+ * @version 1.0
+ * @since 11.12.2020
+ */
+public class HallServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
